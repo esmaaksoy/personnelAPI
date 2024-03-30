93a21370cb0203683fffe3065343117580b280b7
esma <esma.ea.aksoy@gmail.com>
2024-03-30 14:33:46 +0300

    feat: token controller

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -2,12 +2,12 @@
 
 const { mongoose } = require('../configs/dbConnection')
 
-
+//! new ile yazdığım kısım bir instance oluşturma
 const TokenSchema = new mongoose.Schema({
-
+//!Hangi user için token göndereceğimi bilmek için userId field ekledim.
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Personnel', // 'User'
+        ref: 'Personnel',
         required: true,
         index: true,
         unique: true,
