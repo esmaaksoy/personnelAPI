05ded1471a59b8744e3774d4036489b47c738575
esma <esma.ea.aksoy@gmail.com>
2024-03-30 11:48:28 +0300

    feat: department models

--- index.js
+++ index.js
@@ -2,13 +2,29 @@
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 
+require("dotenv").config()
+const PORT = process.env?.PORT || 8000
 
+require("express-async-errors")
+
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+app.use(require('cookie-session')({ secret: process.env.SECRET_KEY }))
+app.use(require("./src/middlewares/queryHandler"))
 app.use(require('./src/middlewares/errorHandler'))
+
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to PERSONNEL API',
+    })
+})
 
 app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
 
-/* ------------------------------------------------------- */
-// Syncronization (must be in commentLine):
+
 // require('./src/helpers/sync')()
--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,3 +1,19 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
+const DepartmentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    collection: "departments",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Department", DepartmentSchema);
